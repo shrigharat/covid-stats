1333ccf1e4a0cabbb054a102091cb662089de867
Shrishail Gharat <gharatshri014@gmail.com>
2020-08-13 22:11:51 +0530

    Updated globe image and title font

--- src/components/head/global-case.jsx
+++ src/components/head/global-case.jsx
@@ -4,7 +4,7 @@ import './global-case.style.css';
 
 export const GlobalCase = props => (
     <div className="global-case">
-        <img className="globe-image" src="https://i.gifer.com/W31X.gif" alt="Globe gif"/>
+        <img className="globe-image" src="https://i.pinimg.com/originals/f3/7e/bb/f37ebbea1f4318dec775a4d705bd7cca.gif" alt="Globe gif"/>
         <div className="head">
             <h1>World Covid Data</h1>
         </div>
